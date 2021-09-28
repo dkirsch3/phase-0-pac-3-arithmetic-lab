4c0e3674f110e242d9cfb3e81a2e844dcee6017e
David <trey3@hotmail.com>
2021-09-28 18:06:54 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+let a , b = Math.floor(Math.random() * 1000)
+
+function add() {
+    
+}
+
+function subtract() {
+    
+}
+
+function multiply() {
+    
+}
+
+function divide() {
+    
+}
+function add(a,b) {
+    return (a + b)
+}
+
+function subtract(a,b) {
+    return (a - b)
+}
+
+function multiply(a,b) {
+    return (a * b)
+}
+
+function divide(a,b) {
+    return (a / b)
+}
+
+function increment(a) {
+    return (a + 1)
+}
+
+function decrement(a) {
+    return (a -1)
+}
+
+function makeInt(n) {
+    let ans = parseInt(n,10)
+    return ans
+}
+
+function preserveDecimal(n) {
+    let ans = parseFloat(n)
+    return ans
+}
